9c3fbf403fba354f6d38064dbb692fdf5d08df78
Edwin <edwin.h.morris@gmail.com>
2014-02-10 13:30:55 -0500

    use hash for DOW values instead of seperate variables

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,36 +1,28 @@
 class CrimesController < ApplicationController
   def show
-    @crime = Crime.all
+    @dow = {
+      'sunday' => 0,
+      'monday' => 0,
+      'tuesday' => 0,
+      'wednesday' => 0,
+      'thursday' => 0,
+      'friday' => 0,
+      'saturday' => 0,
+      'other' => 0
+    }
 
-    @sunday = 0
-    @monday = 0
-    @tuesday = 0
-    @wednesday = 0
-    @thursday = 0
-    @friday = 0
-    @saturday = 0
-    @other = 0;
-
-    @crime.each do |crime|
-      if crime.DAY_WEEK == 'Sunday'
-        @sunday = @sunday + 1
-      elsif crime.DAY_WEEK == 'Monday'
-        @monday = @monday + 1
-      elsif crime.DAY_WEEK == 'Tuesday'
-        @tuesday = @tuesday + 1
-      elsif crime.DAY_WEEK == 'Wednesday'
-        @wednesday = @wednesday + 1
-      elsif crime.DAY_WEEK == 'Thursday'
-        @thursday = @thursday + 1
-      elsif crime.DAY_WEEK = 'Friday'
-        @friday = @friday + 1
-      elsif crime.DAY_WEEK == 'Saturday'
-        @saturday = @saturday + 1
+    Crime.all.each do |crime|
+      if !crime.DAY_WEEK.nil? and @dow[crime.DAY_WEEK.downcase]
+        @dow[crime.DAY_WEEK.downcase] = @dow[crime.DAY_WEEK.downcase] + 1
       else
-        @other = @other + 1
+        @dow['other'] = @dow['other'] + 1
       end
     end
 
-    @neighborhoods = Crime.select('DISTINCT "NEIGHBORHOOD"').map(&:NEIGHBORHOOD).reject(&:nil?)
+    @neighborhoods = Crime.select('DISTINCT "NEIGHBORHOOD"')
+      .map(&:NEIGHBORHOOD)
+      .reject(&:nil?)
+
+    @crime = Crime.all
   end
 end
