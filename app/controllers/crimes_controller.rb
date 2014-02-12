6c994e5212bdf5632fefda2d0bc959f3c4991e19
Edwin <edwin@rapgenius.com>
2014-02-12 17:22:03 -0500

    abstract common words function

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,9 +1,9 @@
 class CrimesController < ApplicationController
   def show
-    @dow = number_of_crimes_by_dow
-
-    @all_words = most_common_word_by_neighborhood
-
+    @common_word_by_neighborhood = most_common_word_by_column 'neighborhood'
+    @common_word_by_lighting = most_common_word_by_column 'lighting'
+    @common_word_by_dow = most_common_word_by_column 'day_week'
+    @crimes_by_dow = number_of_crimes_by_column 'day_week', 7
     @crimes_by_neighborhood = number_of_crimes_by_column 'neighborhood', 10
     @crimes_by_weather = number_of_crimes_by_column 'weather', 10
     @crimes_by_lighting = number_of_crimes_by_column 'lighting', 10
@@ -11,66 +11,81 @@ class CrimesController < ApplicationController
 
   private
 
-  def most_common_word_by_neighborhood
-    words = {}
+  def most_common_word_by_column column_name
+    words_per_unique_column_type = {}
 
-    Crime.select('DISTINCT "neighborhood"')
-      .map(&:neighborhood)
-      .reject(&:nil?)
-      .each do |neighborhood|
+    Crime.unique_column_types(column_name).each do |unique_column_type|
+      words_per_unique_column_type[unique_column_type] = []
 
-      words[neighborhood] = []
-
-      unusualactions = Crime.select(:unusualactions)
-        .where("neighborhood = ?", neighborhood)
-        .map(&:unusualactions)
-        .reject(&:nil?)
-        .each do |sentence|
-
+      Crime.value_per_column_type(
+        column_name,
+        unique_column_type,
+        'unusualactions'
+      ).each do |sentence|
         sentence.split(' ').each do |word|
-          words[neighborhood].push word
+          if filter_filler_word word
+            words_per_unique_column_type[unique_column_type].push word
+          end
         end
       end
     end
 
-    words
-  end
-
-  def number_of_crimes_by_dow
-    dow = {
-      'sunday' => 0,
-      'monday' => 0,
-      'tuesday' => 0,
-      'wednesday' => 0,
-      'thursday' => 0,
-      'friday' => 0,
-      'saturday' => 0,
-      'other' => 0
-    }
-
-    Crime.all.each do |crime|
-      if !crime.day_week.nil? and dow[crime.day_week.downcase]
-        dow[crime.day_week.downcase] = dow[crime.day_week.downcase] + 1
-      else
-        dow['other'] = dow['other'] + 1
-      end
+    words_per_unique_column_type.each do |unique_column_type|
+      words_per_unique_column_type[unique_column_type[0]] =
+        most_common_word_in_array(unique_column_type[1])
     end
-
-    dow
   end
 
-  def number_of_crimes_by_column column_name, number_of_results
+  def most_common_word_in_array array
+    freq = array.inject(Hash.new(0)) { |k, v| k[v] += 1; k }
+    array.max_by { |v| freq[v] }
+  end
+
+  def number_of_crimes_by_column column_name, result_count
     crimes_per_column_type = {}
 
-    Crime.select("DISTINCT \"#{column_name}\"")
-      .map(&:"#{column_name}")
-      .reject(&:nil?)
-      .each do |unique_column_type|
-
+    Crime.unique_column_types(column_name).each do |unique_column_type|
       crime_count = Crime.where("#{column_name} = ?", unique_column_type).count
       crimes_per_column_type[unique_column_type] = crime_count
     end
 
-    crimes_per_column_type.sort_by { |v, k| k }.reverse!.first(number_of_results)
+    crimes_per_column_type.sort_by { |v, k| k }.reverse!.first(result_count)
+  end
+
+  def filter_filler_word word
+    bad_words = [
+      'you',
+      'i',
+      'see',
+      'victim',
+      'suspect',
+      'refer',
+      'to',
+      'the',
+      'can',
+      'unknown',
+      'he',
+      'she',
+      'entered',
+      'went',
+      'let',
+      'got',
+      'there',
+      'motioned',
+      'and',
+      'removed',
+      'trying',
+      'mv',
+      'my',
+      'want',
+      'of',
+      'put',
+      'im',
+      'oh',
+      'give',
+      'in',
+      'do'
+    ]
+    !bad_words.include? word.downcase
   end
 end
--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,3 +1,16 @@
 class Crime < ActiveRecord::Base
   attr_protected
+
+  def self.unique_column_types column_name
+    self.select("DISTINCT \"#{column_name}\"")
+        .map(&:"#{column_name}")
+        .reject(&:nil?)
+  end
+
+  def self.value_per_column_type column_type, column_value, value
+    self.select("#{value}")
+        .where("#{column_type} = ?", column_value)
+        .map(&:"#{value}")
+        .reject(&:nil?)
+  end
 end
