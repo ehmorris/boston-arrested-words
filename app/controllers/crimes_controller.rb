24087caf5cdece68a64124508960fed9aa7a22d2
Edwin <edwin@rapgenius.com>
2014-02-14 11:00:40 -0500

    extract bad words, extract all unusual actions

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,8 +1,8 @@
 class CrimesController < ApplicationController
   def show
-    @common_word_by_neighborhood = most_common_word_by_column 'neighborhood'
-    @common_word_by_lighting = most_common_word_by_column 'lighting'
-    @common_word_by_dow = most_common_word_by_column 'day_week'
+    @common_word_by_neighborhood = most_unusual_action_by_column 'neighborhood'
+    @common_word_by_lighting = most_unusual_action_by_column 'lighting'
+    @common_word_by_dow = most_unusual_action_by_column 'day_week'
     @crimes_by_dow = number_of_crimes_by_column 'day_week', 7
     @crimes_by_neighborhood = number_of_crimes_by_column 'neighborhood', 10
     @crimes_by_weather = number_of_crimes_by_column 'weather', 10
@@ -11,29 +11,35 @@ class CrimesController < ApplicationController
 
   private
 
-  def most_common_word_by_column column_name
-    words_per_unique_column_type = {}
+  def most_unusual_action_by_column column_name
+    most_unusual_actions = all_unusual_actions_by_column column_name
 
-    Crime.unique_column_types(column_name).each do |unique_column_type|
-      words_per_unique_column_type[unique_column_type] = []
+    most_unusual_actions.each do |unique_column_values_and_words|
+      most_unusual_actions[unique_column_values_and_words[0]] =
+        most_common_word_in_array(unique_column_values_and_words[1])
+    end
+  end
+
+  def all_unusual_actions_by_column column_name
+    words_per_unique_column_value = {}
+
+    Crime.unique_column_values(column_name).each do |unique_column_value|
+      words_per_unique_column_value[unique_column_value] = []
 
       Crime.value_per_column_type(
         column_name,
-        unique_column_type,
+        unique_column_value,
         'unusualactions'
       ).each do |sentence|
         sentence.split(' ').each do |word|
           if filter_filler_word word
-            words_per_unique_column_type[unique_column_type].push word
+            words_per_unique_column_value[unique_column_value].push word
           end
         end
       end
     end
 
-    words_per_unique_column_type.each do |unique_column_type|
-      words_per_unique_column_type[unique_column_type[0]] =
-        most_common_word_in_array(unique_column_type[1])
-    end
+    words_per_unique_column_value
   end
 
   def most_common_word_in_array array
@@ -44,7 +50,7 @@ class CrimesController < ApplicationController
   def number_of_crimes_by_column column_name, result_count
     crimes_per_column_type = {}
 
-    Crime.unique_column_types(column_name).each do |unique_column_type|
+    Crime.unique_column_values(column_name).each do |unique_column_type|
       crime_count = Crime.where("#{column_name} = ?", unique_column_type).count
       crimes_per_column_type[unique_column_type] = crime_count
     end
@@ -53,7 +59,6 @@ class CrimesController < ApplicationController
   end
 
   def filter_filler_word word
-    bad_words = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"]
-    !bad_words.include? word.downcase
+    !Crime.stopwords.include? word.downcase
   end
 end
--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,7 +1,7 @@
 class Crime < ActiveRecord::Base
   attr_protected
 
-  def self.unique_column_types column_name
+  def self.unique_column_values column_name
     self.select("DISTINCT \"#{column_name}\"")
         .map(&:"#{column_name}")
         .reject(&:nil?)
@@ -12,5 +12,9 @@ class Crime < ActiveRecord::Base
         .where("#{column_type} = ?", column_value)
         .map(&:"#{value}")
         .reject(&:nil?)
+  end
+
+  def self.stopwords
+    ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"]
   end
 end
